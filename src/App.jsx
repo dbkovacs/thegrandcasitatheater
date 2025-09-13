7195f7f83fbc25f711a9c1da5e5d1cc01faf7243
dbkovacs <dbkovacs@gmail.com>
2025-09-13 10:44:50 -0600

    move the page content out of App.jsx and into their own dedicated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,9 @@
 // File: src/App.jsx
 // This is the main application component. It handles routing to display the correct page.
-// NOTE: All page components are temporarily in this file to resolve a build issue.
 import React from 'react';
-
-// --- Page Components (temporary location) ---
-
-const SignUpPage = () => {
-    // This component will be moved to its own file later
-    return (
-        <div className="text-center p-8">
-            <h1 className="text-4xl font-cinzel mb-4">Pick a Movie</h1>
-            <p>The form to sign up to host a movie will be built here.</p>
-        </div>
-    );
-};
-
-const AdminPage = () => {
-    // This component will be moved to its own file later
-    return (
-        <div className="text-center p-8">
-            <h1 className="text-4xl font-cinzel mb-4">Admin Page</h1>
-            <p>The admin dashboard will be built here.</p>
-        </div>
-    );
-};
-
-const InvitationPage = () => {
-    // This component will be moved to its own file later
-    return (
-        <div className="text-center p-8">
-            <h1 className="text-4xl font-cinzel mb-4">A Knight's Tale</h1>
-            <p>This is the main invitation page. Content will be built out here.</p>
-        </div>
-    );
-};
-
-
-// --- Main App ---
+import SignUpPage from './SignUpPage';
+import AdminPage from './AdminPage';
+import InvitationPage from './InvitationPage';
 
 function App() {
     const [currentPage, setCurrentPage] = React.useState('/');
@@ -76,5 +43,4 @@ function App() {
 }
 
 export default App;
-// END - 2025-09-12 18:25 PM
-
+// END - 2025-09-13 10:42 AM
