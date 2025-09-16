9a7b455f0ee39b1a836e7cad9baaaa9ded1063f3
dbkovacs <dbkovacs@gmail.com>
2025-09-16 14:40:30 -0600

    2:40

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+// src/lib/utils.ts
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+// Build Date: 2025-09-16 02:39 PM
